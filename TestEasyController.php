11b1a56b78f7b60a32923c4e6157aa8904446a20
vinicius-easy <106549769+vinicius-easy@users.noreply.github.com>
2022-08-29 08:44:41 -0300

    Add files via upload

---
+++ TestEasyController.php
@@ -0,0 +1,22 @@
+<?php
+namespace Application\controllers;
+
+use Application\core\Controller;
+use Application\models\helper\Header;
+
+class TestEasyController extends Controller
+{
+    public function listing()
+    {
+        $header = new Header();
+        $header->setTitle("Exemplo Titulo");
+        $header->setNamePage("Exemplo Nome da Página");
+
+        $data = [
+            "header" => $header,
+            "nome" => 'nome'
+        ];
+
+        $this->viewSystem('teste/listing',$data);
+    }
+}
